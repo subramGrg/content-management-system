ac9b1bea75189db48d7c8b24c7d5b793b4526195
subram <sups88@hotmail.com>
2018-02-16 06:59:25 +1100

    heroku test

--- server.js
+++ server.js
@@ -13,8 +13,7 @@ const PORT = process.env.PORT || 3000;
 // create app
 const app = express();
 // static file location
-// app.use(express.static(`${__dirname}/dist`));
-app.get("/", (req, res) => (res.send(PORT)));
+app.use(express.static(`${__dirname}/dist`));
 
 app.listen(PORT, () => {
     console.log(`server is up in ${PORT}`);
