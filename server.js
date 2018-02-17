d3e8f7e233bf0e20c58370288592559f93d021d4
subram <sups88@hotmail.com>
2018-02-17 18:08:50 +1100

    add browser history fallback

--- server.js
+++ server.js
@@ -1,18 +1,11 @@
-// const express = require("express");
-// const app = express();
-//
-// app.get("/", (req, res) => (res.send("hello world")));
-//
-// app.listen(2000, () => (
-//     console.log("Listening on port 3000")
-// ));
-
 const express = require("express");
+const history = require("connect-history-api-fallback");
 
 const PORT = process.env.PORT || 3000;
 // create app
 const app = express();
 // static file location
+app.use(history()); // this gets executed first
 app.use(express.static(`${__dirname}/dist`));
 
 app.listen(PORT, () => {
